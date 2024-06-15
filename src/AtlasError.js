8d3e9578e4864f7c0015b236b475c9130969c441
Erfan Mohammad Karimi Zade <karimierfan74@gmail.com>
2024-06-16 02:30:01 +0330

    Update -> Custom errors added

---
+++ src/AtlasError.js
@@ -0,0 +1,17 @@
+class AtlasError extends Error {
+  constructor(message, type, tips, data) {
+    super(message);
+
+    this.name = "AtlasError -> " + type
+    if (tips) {
+      this.tips = tips
+    }
+    if (data) {
+      this.data = data
+    }
+  }
+}
+
+export function raiseError(message, type, tips, data) {
+  throw new AtlasError(message, type, tips, data)
+}
